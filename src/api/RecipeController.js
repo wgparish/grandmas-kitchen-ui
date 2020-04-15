3a4b5c19db7050473c56cf4d5f3b86a63fd3f987
William Parish <wparish@finlistics.com>
2020-04-14 21:21:14 -0500

    Added methods for Recipe Controller

--- src/api/RecipeController.js
+++ src/api/RecipeController.js
@@ -1,12 +1,60 @@
 import api from "../api/ApiBackend";
 
 class RecipeController {
-    static recipeAdd = "/recipe/secure/add";
-    static recipeUpdate = "/recipe/secure/update";
-    static recipeDeactivate = "/recipe/secure/deactivate";
-    static recipeView = "/recipe/secure/view";
-    static recipeList = "/recipe/secure/list";
-    static recipeFavoriteList = "/recipe/secure/list/favorite";
+    static recipeAddUrl = "/recipe/secure/add";
+    static recipeUpdateUrl = "/recipe/secure/update";
+    static recipeDeactivateUrl = "/recipe/secure/deactivate";
+    static recipeViewUrl = "/recipe/secure/view";
+    static recipeListUrl = "/recipe/secure/list";
+    static recipeFavoriteListUrl = "/recipe/secure/list/favorite";
+
+    static recipeAdd = async (recipeAddRequest) => {
+        return api.post(this.recipeAddUrl, recipeAddRequest, {withCredentials:true});
+    }
+
+    static recipeUpdate = async (recipeUpdateRequest) => {
+        return api.put(this.recipeUpdateUrl, recipeUpdateRequest, {withCredentials:true});
+    }
+
+    static recipeDeactivate = async (recipeId) => {
+        return api.delete(this.recipeDeactivateUrl, {
+            params: {
+                recipeId: recipeId
+            },
+            withCredentials: true
+        });
+    }
+
+    static recipeView = async (groupId, cookbookId, recipeId) => {
+        return api.get(this.recipeViewUrl, {
+            params: {
+                groupId: groupId,
+                cookbookId: cookbookId,
+                recipeId: recipeId
+            },
+            withCredentials: true
+        });
+    }
+
+    static recipeList = async (groupId, cookbookId) => {
+        return api.get(this.recipeListUrl, {
+            params: {
+                groupId: groupId,
+                cookbookId: cookbookId
+            },
+            withCredentials: true
+        });
+    }
+
+    static recipeFavoriteList = async (groupId, cookbookId) => {
+        return api.get(this.recipeFavoriteListUrl, {
+            params: {
+                groupId: groupId,
+                cookbookId: cookbookId
+            },
+            withCredentials: true
+        });
+    }
 
 }
 
