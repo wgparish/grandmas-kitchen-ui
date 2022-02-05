e2296922001bb8554c1734c977d6d641875e299a
William Parish <william.g.parish@gmail.com>
2022-02-04 20:35:11 -0600

    Added Steps & addition & removal

--- src/views/Recipe/CreateRecipePage/CreateRecipeWizardSteps/RecipeStepList.js
+++ src/views/Recipe/CreateRecipePage/CreateRecipeWizardSteps/RecipeStepList.js
@@ -9,6 +9,8 @@ import WizardStep from "../../../../components/Wizard/WizardStep";
 import GridContainer from "../../../../components/Grid/GridContainer";
 import GridItem from "../../../../components/Grid/GridItem";
 import CustomInput from "../../../../components/CustomInput/CustomInput";
+import { OutdoorGrill } from "@material-ui/icons";
+import Button from "../../../../components/CustomButtons/Button";
 const style = {
   infoText: {
     fontWeight: "300",
@@ -27,177 +29,74 @@ class RecipeStepList extends WizardStep {
   constructor(props) {
     super(props);
     this.state = {
-      firstname: "",
-      firstnameState: "",
-      lastname: "",
-      lastnameState: "",
-      phone: "",
-      phoneState: ""
+      stepList: []
     };
   }
+
   sendState() {
     return this.state;
   }
 
-  // function that verifies if a string has a given length or not
-  verifyLength(value, length) {
-    if (value.length >= length) {
-      return true;
-    }
-    return false;
+  addStep() {
+    console.log(this.state);
+    console.log("=================");
+    const tempStepList = [...this.state.stepList];
+    tempStepList.push({ stepName: "" });
+    this.setState({ stepList: tempStepList });
   }
 
-  change(event, stateName, type, stateNameEqualTo) {
-    switch (type) {
-      case "length":
-        if (this.verifyLength(event.target.value, stateNameEqualTo)) {
-          this.setState({ [stateName + "State"]: "success" });
-        } else {
-          this.setState({ [stateName + "State"]: "error" });
-        }
-        break;
-      default:
-        break;
-    }
-    this.setState({ [stateName]: event.target.value });
+  removeStep() {
+    console.log(this);
+    console.log("=================");
+    const tempStepList = [...this.state.stepList];
+    tempStepList.pop();
+    this.setState({ stepList: tempStepList });
   }
 
-  isValidated() {
-    return (
-      this.state.firstnameState === "success" &&
-      this.state.lastnameState === "success"
-    );
+  change(event, index) {
+    const tempStepList = [...this.state.stepList];
+    tempStepList[index][event.target.id] = event.target.value;
+    console.log("Hardware Item: " + event.target.value);
+    this.setState({ stepList: tempStepList });
   }
 
   render() {
-    const { classes, validationErrors } = this.props;
-
-    let firstNameError = validationErrors.filter(validationError =>
-      validationError.includes("First Name")
-    );
-    let lastNameError = validationErrors.filter(validationError =>
-      validationError.includes("Last Name")
-    );
-    let phoneNumberError = validationErrors.filter(validationError =>
-      validationError.includes("Phone Number")
-    );
-    let firstNameErrorExists = firstNameError.length > 0;
-    let lastNameErrorExists = lastNameError.length > 0;
-    let phoneNumberErrorExists = phoneNumberError.length > 0;
+    const { classes } = this.props;
 
     return (
       <GridContainer justify="center">
         <GridItem xs={12} sm={12}>
-          <h4 className={classes.infoText}>
-            Let's start with the basic information
-          </h4>
+          <h4 className={classes.infoText}>How are we cooking this recipe?</h4>
         </GridItem>
-
         <GridItem xs={12} sm={6}>
-          <CustomInput
-            success={
-              this.state.firstnameState === "success" && !firstNameErrorExists
-            }
-            error={
-              this.state.firstnameState === "error" || firstNameErrorExists
-            }
-            labelText={
-              firstNameErrorExists ? (
-                <span>
-                  First Name <small>({firstNameError[0]})</small>
-                </span>
-              ) : (
-                <span>
-                  First Name <small>(required)</small>
-                </span>
-              )
-            }
-            id="firstname"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => {
-                this.change(event, "firstname", "length", 3);
-                // registrationDatta.firstName = event.target.value;
-              },
-              endAdornment: (
-                <InputAdornment
-                  position="end"
-                  className={classes.inputAdornment}
-                >
-                  <Face className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-          <CustomInput
-            success={
-              this.state.lastnameState === "success" && !lastNameErrorExists
-            }
-            error={this.state.lastnameState === "error" || lastNameErrorExists}
-            labelText={
-              lastNameErrorExists ? (
-                <span>
-                  Last Name <small>({lastNameError[0]})</small>
-                </span>
-              ) : (
-                <span>
-                  Last Name <small>(required)</small>
-                </span>
-              )
-            }
-            id="lastname"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => {
-                this.change(event, "lastname", "length", 3);
-                // registrationData.lastName = event.target.value;
-              },
-              endAdornment: (
-                <InputAdornment
-                  position="end"
-                  className={classes.inputAdornment}
-                >
-                  <RecordVoiceOver className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
-          <CustomInput
-            error={phoneNumberErrorExists}
-            labelText={
-              phoneNumberErrorExists ? (
-                <span>
-                  Phone <small>(optional)</small>
-                </span>
-              ) : (
-                <span>
-                  Phone <small>({phoneNumberError[0]})</small>
-                </span>
-              )
-            }
-            id="phone"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              onChange: event => {
-                this.change(event, "phone", "", null);
-                // registrationData.phoneNumber = event.target.value;
-              },
-              endAdornment: (
-                <InputAdornment
-                  position="end"
-                  className={classes.inputAdornment}
-                >
-                  <RecordVoiceOver className={classes.inputAdornmentIcon} />
-                </InputAdornment>
-              )
-            }}
-          />
+          {this.state.stepList.map((stepList, i) => {
+            return (
+              <CustomInput
+                labelText={<span>Step Name</span>}
+                id="stepName"
+                formControlProps={{
+                  fullWidth: true
+                }}
+                inputProps={{
+                  onChange: event => {
+                    this.change(event, i);
+                  },
+                  endAdornment: (
+                    <InputAdornment
+                      position="end"
+                      className={classes.inputAdornment}
+                    >
+                      <OutdoorGrill className={classes.inputAdornmentIcon} />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            );
+          })}
+        </GridItem>
+        <GridItem xs={12} sm={12}>
+          <Button onClick={this.addStep.bind(this)}>Add Step</Button>
+          <Button onClick={this.removeStep.bind(this)}>Remove Step</Button>
         </GridItem>
       </GridContainer>
     );
