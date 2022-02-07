057742b5c7f928bc51ba0e6999c1b8f7224009c8
William Parish <william.g.parish@gmail.com>
2022-02-06 22:11:36 -0600

    Updating Base url

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -21,6 +21,6 @@
  *     > once that image id is obtained, then the image can be downloaded from the general images endpoint using the image id as a parameter
  */
 
-const apiBaseURL = "http://localhost:8080/";
+const apiBaseURL = "http://68.183.19.115:8080/";
 
 export default apiBaseURL;
